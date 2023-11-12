f1ccce247bbad430dd462162329a761df06eb852
Garrett Ourth <garrettourth@gmail.com>
2023-11-11 20:59:53 -0600

    pre rendering and dynamic routes

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -11,6 +23,20 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Hello, my name is Garrett. I am married to my lovely wife Jill and we have a beautiful daughter named Lila. I love my family and the Kansas City Chiefs!</p>
         
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
